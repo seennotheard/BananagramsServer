999d9f2a18d21ce8a2ed923f51d1a447fd9f3306
Sean Ma <seanm3266@gmail.com>
2017-05-13 23:10:02 -0400

    client

--- BananagramsClient/src/BananagramsClientThread.java
+++ BananagramsClient/src/BananagramsClientThread.java
@@ -1,4 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.*;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class BananagramsClientThread extends Thread {
 	private Socket socket = null;
@@ -9,6 +16,20 @@ public class BananagramsClientThread extends Thread {
 	}
 	
 	public void run() {
-		
+		try (
+	            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+	            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	        ) {
+				Scanner scanner = new Scanner(System.in);
+				String str = scanner.next();
+	            while ((str = in.readLine()) != null) {
+	                out.println(str);
+	                //add method for disconnect
+	            }
+	            scanner.close();
+	            socket.close();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
 	}
 }
